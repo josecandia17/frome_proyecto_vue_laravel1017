f6d1047d9e374fc3dd6bb2fe549755aefbe1a706
jose candia <carlosjose171509@gmail.com>
2024-02-12 21:05:56 -0400

    from-proyecto-vue-laravel

---
+++ src/views/LoginView.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        {{ usuario }}
+        <h1>Login</h1>
+        <label for="e">Ingresar Correo</label>
+        <input type="email" v-model="usuario.email">
+        <br>
+        <label>Ingresar Contraseña</label>
+        <input type="password" v-model="usuario.password">
+        <br>
+        <input type="button" value="Ingresar" v-on:click="funIngresar()">
+    </div>
+        
+</template>
+
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+
+    // variables 
+    const correo= ref ("micorreo@gmail.com");
+
+    const clave=ref ("mipassword");
+
+    const usuario = ref ({ email: "", password:""})
+    //metodos
+    function funIngresar(){
+        axios.post("http://127.0.0.1:8000/api/v1/auth/login", usuario.value).then(
+            (res) => {
+                console.log(res)
+            } 
+        )
+    }
+
+</script>
+
+
